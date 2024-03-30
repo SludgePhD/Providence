98d7f1702155304f5b231d61e536f2029363c25a
Sludge <96552222+SludgePhD@users.noreply.github.com>
2024-03-30 18:04:51 +0100

    zarupdate again

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{cmp, io};
 use pawawwewism::{promise, Promise, PromiseHandle, Worker};
 use providence_io::data::{FaceData, PersistentId, TrackingMessage};
 use providence_io::net::Publisher;
+use tracing::debug_span;
 use triangulate::{Eye, Triangulator};
 use zaru::detection::Detector;
 use zaru::face::detection::ShortRangeNetwork;
@@ -19,7 +20,6 @@ use zaru::linalg::{vec3, Quat};
 use zaru::num::TotalF32;
 use zaru::procrustes::ProcrustesAnalyzer;
 use zaru::rect::RotatedRect;
-use zaru::timer::{FpsCounter, Timer};
 use zaru::video::webcam::{ParamPreference, Webcam, WebcamOptions};
 
 const TIMESTAMP_OFFSET: u32 = u32::MAX - 10_000_000; // 10 seconds before overflow
@@ -39,7 +39,6 @@ fn main() -> anyhow::Result<()> {
     let reference_time = Instant::now();
     let mut publisher = Publisher::spawn()?;
     let mut message_queue = VecDeque::new();
-    let mut fps = FpsCounter::new("webcam");
     loop {
         // To avoid wasting CPU, we only perform processing when there is a client connected.
         // Ideally we'd also clear the face tracking state, but that's kinda difficult to do.
@@ -70,8 +69,6 @@ fn main() -> anyhow::Result<()> {
         });
         message_queue.push_back(message_handle);
 
-        fps.tick_with(webcam.timers());
-
         if let Some(handle) = message_queue.front() {
             if !handle.will_block() {
                 let mut message = match message_queue.pop_front().unwrap().block() {
@@ -98,10 +95,6 @@ struct AssemblerParams {
 }
 
 fn assembler() -> Result<Worker<AssemblerParams>, io::Error> {
-    let mut fps = FpsCounter::new("assembler");
-    let t_procrustes = Timer::new("procrustes");
-    let t_triangulate = Timer::new("triangulate");
-
     let mut procrustes_analyzer = ProcrustesAnalyzer::new(mediapipe::reference_positions());
     let mut tri = Triangulator::new();
 
@@ -112,7 +105,7 @@ fn assembler() -> Result<Worker<AssemblerParams>, io::Error> {
                 return;
             };
 
-            let procrustes_result = t_procrustes.time(|| {
+            let procrustes_result = debug_span!("procrustes").in_scope(|| {
                 procrustes_analyzer.analyze(face_landmark.mesh_landmarks().map(|lm| {
                     // Flip Y to bring us to canonical 3D coordinates (where Y points up).
                     // Only rotation matters, so we don't have to correct for the added
@@ -127,12 +120,12 @@ fn assembler() -> Result<Worker<AssemblerParams>, io::Error> {
             let head_rotation = Quat::from_rotation_xyz(-x, y, -z);
             let head_rotation_inv = head_rotation.conjugate();
 
-            let guard = t_triangulate.start();
-            let left_eye =
-                tri.triangulate_eye(&face_landmark, &image, Eye::Left, head_rotation_inv);
-            let right_eye =
-                tri.triangulate_eye(&face_landmark, &image, Eye::Right, head_rotation_inv);
-            drop(guard);
+            let (left_eye, right_eye) = debug_span!("triangulate").in_scope(|| {
+                (
+                    tri.triangulate_eye(&face_landmark, &image, Eye::Left, head_rotation_inv),
+                    tri.triangulate_eye(&face_landmark, &image, Eye::Right, head_rotation_inv),
+                )
+            });
 
             // Mirror the whole image, so that the eyes match what the user does.
             let (right_eye, left_eye) = (left_eye.flip_horizontal(), right_eye.flip_horizontal());
@@ -158,8 +151,6 @@ fn assembler() -> Result<Worker<AssemblerParams>, io::Error> {
                     right_eye: right_eye.into_message(),
                 }],
             });
-
-            fps.tick_with([&t_procrustes, &t_triangulate]);
         })
 }
 
@@ -174,9 +165,6 @@ struct FaceTrackParams {
 /// - Compute facial landmarks and track their positions across frames
 /// - Copy the eye's regions from the image and send them to the eye tracking workers
 fn face_track_worker() -> Result<Worker<FaceTrackParams>, io::Error> {
-    let mut fps = FpsCounter::new("tracker");
-    let t_total = Timer::new("total");
-
     let mut detector = Detector::new(ShortRangeNetwork);
     let mut estimator = Estimator::new(FaceMeshV2);
     estimator.set_filter(LandmarkFilter::new(
@@ -189,8 +177,6 @@ fn face_track_worker() -> Result<Worker<FaceTrackParams>, io::Error> {
     Worker::builder()
         .name("face tracker")
         .spawn(move |FaceTrackParams { image, landmarks }| {
-            let guard = t_total.start();
-
             if tracker.roi().is_none() {
                 // Zoom into the camera image and perform detection there. This makes outer
                 // edges of the camera view unusable, but significantly improves the tracking
@@ -205,7 +191,7 @@ fn face_track_worker() -> Result<Worker<FaceTrackParams>, io::Error> {
                 {
                     let rect = target.bounding_rect().move_by(view_rect.top_left());
                     let rect = RotatedRect::new(rect, target.angle());
-                    log::trace!("start tracking face at {:?}", rect);
+                    tracing::trace!("start tracking face at {:?}", rect);
                     tracker.set_roi(rect);
                 }
             }
@@ -213,14 +199,6 @@ fn face_track_worker() -> Result<Worker<FaceTrackParams>, io::Error> {
             if let Some(res) = tracker.track(&image) {
                 landmarks.fulfill((res.estimate().clone(), image));
             }
-
-            drop(guard);
-            fps.tick_with(
-                [&t_total]
-                    .into_iter()
-                    .chain(detector.timers())
-                    .chain(tracker.timers()),
-            );
         })
 }
 
